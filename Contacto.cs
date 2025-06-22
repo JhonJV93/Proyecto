81c23cfca8b5ce65fd5dbdc286e92857fef356ef
JhonJV93 <jj.velascov@uea.edu.ec>
2025-06-22 01:02:02 -0500

    PROYECTO

---
+++ Contacto.cs
@@ -0,0 +1,30 @@
+﻿// Clase abstracta que representa un contacto genérico
+public abstract class Contacto
+{
+    // Encapsulamiento: campos privados con propiedades publicas
+    private string nombre;
+    private string telefono;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public string Telefono
+    {
+        get { return telefono; }
+        set { telefono = value; }
+    }
+
+    // Constructor
+
+    public Contacto(string nombre, string telefono)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    // Abtraccion: metodo que sera implementado en clases hijas
+    public abstract void Mostrar();
+}
